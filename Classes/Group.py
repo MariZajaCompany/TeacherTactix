45ab38c66ae21f96c75550ca204520912c59d327
MalgiGie <gyurlfriend123@gmail.com>
2024-02-27 21:57:08 +0100

    modified:   Classes/Schedule.py

--- Classes/Group.py
+++ Classes/Group.py
@@ -1,45 +1,95 @@
 from Classes.ClassInSchool import ClassInSchool
+#from Classes.Subgroup import Subgroup
 class Group:
     def __init__(self, day, hour):
-        self.list_of_classes = []
-        self.number_of_children = 0
+        self.grade_attendance = [0,0,0,0]
+        self.subgroups = [[],[],[],[]]
         self.day = day
         self.hour = hour
         self.teacher = ""
         self.room = ""
 
     def get_list_of_classes(self):
-        return self.list_of_classes
-        
-    def get_number_of_children(self):
-        return self.number_of_children
+        list_of_classes = []
+        for i in range(4):
+            list_of_classes += self.subgroups[i]
+        return list_of_classes
+    
+    def get_subgroup(self, grade):
+        return self.subgroups[grade]
+    
+    def get_subgroups(self):
+        return self.subgroups
+    
+    def get_attendance(self, *arguments): # any number of arguments
+        return sum(self.grade_attendance)
+    
+    def get_grade_attendance(self, *arguments): # any number of arguments
+        return sum(self.grade_attendance)
+    
+    def set_time(self, day, hour): #update of a group size and removing absent classes
+        self.day = day
+        self.hour = hour
+        self.attendance = 0
+        for c in self.get_list_of_classes():
+            class_attendance = c.get_attendance(day, hour)
+            self.grade_attendance[c.get_grade()] += class_attendance
+            if class_attendance == 0:
+                self.remove_class(c)
+    
+    def remove_class(self, c):
+        grade = c.get_grade()
+        for c in self.subgroups[grade]:
+            if c.get_class_name() == c.get_class_name():
+                self.subgroups[grade].remove(c)
+                break
+        self.grade_attendance[grade] -= c.get_attendance(self.day, self.hour)
 
     def add_children(self, object):
-        if isinstance(object, Group):
-            if self.number_of_children + object.get_attendance(self.day,self.hour) <= 25:
-                self.list_of_classes += object.get_list_of_classes()
-                self.number_of_children += object.get_attendance(self.day,self.hour)
+        if isinstance(object, Group): # TO DO
+            if self.get_attendance() + object.get_attendance() <= 25:
+                for grade in range(4):
+                    self.subgroups[grade] += object.get_subgroup(grade)
+                self.grade_attendance[grade] += object.get_attendance()
                 return True # group added successfully
             else:
                 return False  
         elif isinstance(object, ClassInSchool):
-            if self.number_of_children + object.get_attendance(self.day,self.hour) <= 25:
-                self.list_of_classes.append(object)
-                self.number_of_children += object.get_attendance(self.day,self.hour)
+            grade = -1
+            for i in range(4):
+                if self.grade_attendance[i] != 0:
+                    grade = i
+                    break
+
+            if self.get_attendance() + object.get_attendance(self.day,self.hour) <= 25:
+                grade = object.get_grade()
+                self.subgroups[grade].append(object)
+                self.grade_attendance[grade] += object.get_attendance(self.day,self.hour)
                 return True # class added successfully
+            
+            elif self.get_grade_attendance() + object.get_attendance(self.day,self.hour) <= 25 and grade == object.get_grade():
+                self.subgroups[grade].append(object)
+                self.grade_attendance[grade] += object.get_attendance(self.day,self.hour)
+                grade += 1
+                while grade < 4 and self.get_attendance() + c.get_attendance(self.day,self.hour) > 25:
+                    tmp_subgroups = self.subgroups[grade]
+                    for c in tmp_subgroups:
+                        self.subgroups[grade].remove(c)
+                        self.grade_attendance[grade] -= c.get_attendance(self.day,self.hour)
+                        # if self.get_attendance() + c.get_attendance(self.day,self.hour) > 25:
+                        #     self.subgroups[grade].remove(c)
+                        #     self.grade_attendance[grade] -= c.get_attendance(self.day,self.hour)
+                        # else:
+                        #     break
+                    grade += 1
+                return True # class exchanged successfully
             else:
                 return False
-            
-    def get_attendance(self, day, hour):
-        attendance = 0
-        for c in self.list_of_classes:
-            attendance += c.get_attendance(day, hour)
-        return attendance
             
     def display_group(self):
         #print(f"Day: {self.day}")
         #print(f"Hour: {self.hour}")
-        print(f"Number of Children: {self.number_of_children}")
+        print(f"Number of Children: {self.get_attendance()}")
         print(f"Teacher: {self.teacher}")
         print(f"Room: {self.room}")
         print("Classes:")
--- Classes/Schedule.py
+++ Classes/Schedule.py
@@ -1,12 +1,7 @@
 
 from Classes.ClassInSchool import ClassInSchool
 from Classes.Group import Group
-
-def sorting_key(object, day, hour):
-    if isinstance(object, Group):
-        return object.get_number_of_children()
-    elif isinstance(object, ClassInSchool):
-        return object.get_attendance(day, hour)
+import copy
 
 class Schedule:
     def __init__(self):
@@ -37,50 +32,72 @@ class Schedule:
 
     def create_groups(self, all_classes):
 
-        for day in range(5):
+        for day in range(1):
             prev_groups = [[], [], [], []]
             for hour in range(5):
+                present_groups = [[], [], [], []]
 
-                current_groups = [[], [], [], []]
-                present_classes = [[], [], [], []]
-
-                
+                # check attendance
                 for school_class in all_classes:
-                    if school_class.get_attendance(day, hour) > 0:
-                        check = True
+                    class_attendance = school_class.get_attendance(day, hour)
+                    if class_attendance > 0:
+                        present_groups[school_class.get_grade()].append(school_class)
+                
+                # update group size and elements
+                for i in range(4):
+                    for group in prev_groups[i]:
+                        group.set_time(day, hour)
+                        for school_class in group.get_list_of_classes():
+                            for object in present_groups[i]:
+                                if isinstance(object, Group): # TO DO
+                                    object.remove_class(school_class) 
+                                elif isinstance(object, ClassInSchool):
+                                    if school_class.get_class_name() == object.get_class_name():
+                                        present_groups[i].remove(object)
+
+                        # removing classes from too big subgroups
                         for grade in range(4):
-                            for group in prev_groups[grade]:
-                                if school_class in group.get_list_of_classes():
-                                    check = False
-                        if (check): present_classes[school_class.class_grade].append(school_class)
+                            subgroup = sorted(group.get_subgroup(grade), key=lambda school_class: school_class.get_attendance(day, hour), reverse=True)
+                            for school_class in subgroup:
+                                if group.get_grade_attendance(grade) > 25:
+                                    group.remove_class(school_class)
+                                    present_groups[school_class.get_grade()].append(school_class)
+                                else:
+                                    break
+                        
+                        # removing classes from too big subgroups
+                        subgroups = sorted(group.get_subgroups(), key=lambda subgroup: sum(c.get_attendance(day, hour) for c in subgroup), reverse=True)
+                        for subgroup in subgroups:
+                            if group.get_attendance(grade) > 25:
+                                for school_class in group.get_subgroup(grade):
+                                    group.remove_class(school_class)
+                                    present_groups[school_class.get_grade()].append(school_class)
+                            else:
+                                break
 
+                new_groups = [[], [], [], []]
+                
                 for grade in range(4):
+
                     for group in prev_groups[grade]:
-                        for classInGroup in group.get_list_of_classes():
-                            if classInGroup in present_classes[grade]:
-                                present_classes[grade].remove(classInGroup)
-                        present_classes[grade].append(group)
+                        present_groups[grade].append(group)
+                    
+                    present_groups[grade] = sorted(present_groups[grade], key=lambda school_class: school_class.get_attendance(day, hour), reverse=True)
+
+                    while present_groups[grade]: # dzialanie tej petli jest specjalne
+                        for existing_group in present_groups[grade]:
+                            group = Group(day, hour)
+                            present_groups[grade].remove(existing_group)
+                            group.add_children(existing_group)
+                            for another_group in present_groups[grade]:
+                                if group.add_children(another_group):
+                                    present_groups[grade].remove(another_group)
+                            new_groups[grade].append(group)
 
                 for grade in range(4):
-                    
-                    present_classes[grade] = sorted(present_classes[grade], key=lambda school_class: school_class.get_attendance(day, hour), reverse=True)
-                    
-                    
+                    self.add_groups(new_groups[grade], day, hour)
 
-                    while present_classes[grade]: # dzialanie tej petli jest specjalne
-                            for school_class in present_classes[grade]:
-                                group = Group(day, hour)
-                                present_classes[grade].remove(school_class)
-                                group.add_children(school_class)
-                                for inna_ClassInSchool in present_classes[grade]:
-                                    if group.add_children(inna_ClassInSchool):
-                                        present_classes[grade].remove(inna_ClassInSchool)
-                                current_groups[grade].append(group)
-
-                for grade in range(4):
-                    self.add_groups(current_groups[grade], day, hour)
-
-                prev_groups = current_groups
+                prev_groups = copy.deepcopy(new_groups) #aktualizacja prev_groups
         
 
     def save_as(self):
