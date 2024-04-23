92ec7804e6e6a48e263745bb611f22bdd771c01a
MariZaja <m.zajaczkowska02@gmail.com>
2024-04-23 23:38:45 +0200

    resolve conflicts

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-import ControllerGUI
+from Classes.Schedule import Schedule
+from Classes.ClassInSchool import ClassInSchool
 
-if __name__ == "__main__":
-    ControllerGUI.start_graphic_user_interface()
+import os
+import re
+
+def create_classes_from_folder(directory):
+    filePrefix = 'rozklad_'
+    csvFilesList = [plik for plik in os.listdir(directory) if plik.startswith(filePrefix) and plik.endswith('.csv')]
+    createdClasses = []
+
+    for fileName in sorted(csvFilesList):
+        result = re.match(r'rozklad_(\d+)(\w)\.csv', fileName)
+        if result:
+            number, letter = result.groups()
+            classObject = ClassInSchool(int(number), letter)
+            createdClasses.append(classObject)
+            csvFilePath = os.path.join("Data", fileName)
+            classObject.read_from_csv(csvFilePath)
+        else:
+            print(f"Wrong file name format: {fileName}")
+    return createdClasses
+
+
+
+if __name__ == '__main__':
+    
+    data_folder = "Data"
+
+    all_classes = create_classes_from_folder(data_folder)
+
+    # utworzenie harmonogramu
+    schedule = Schedule() 
+    schedule.new_create_groups(all_classes)
+    schedule.display_schedule()
+    #schedule.save_as()
